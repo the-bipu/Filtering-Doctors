61298712043bf4b17e0d9b983734664a0b20093b
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-20 18:10:41 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home, Service } from './pages'
@@ -7,14 +7,16 @@ function App() {
 
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Service />} />
-        <Route path="/doctors/:pageNumber" element={<Home />} />
-        <Route path="/doctors" element={<Home />} />
-        <Route path="/Hospital" element={<Service />} />
-        <Route path="/About" element={<Service />} />
-        <Route path="/Contact" element={<Service />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors/:pageNumber" element={<Home />} />
+          <Route path="/doctors" element={<Home />} />
+          <Route path="/Hospital" element={<Service />} />
+          <Route path="/About" element={<Service />} />
+          <Route path="/Contact" element={<Service />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
