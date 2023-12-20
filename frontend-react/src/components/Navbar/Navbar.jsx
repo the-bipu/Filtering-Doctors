7390dc366067aaa43da5f0b5377dbd4d80f004a5
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-20 21:17:39 +0530

    added data and components

---
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Navbar.scss';
+
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+  
+  const toggleNavbar = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+
+      <div className={`navbar--main ${isActive ? 'active' : ''}`}>
+      <div className='container'>
+            <a href="/" className="logo"> <i className="fas fa-lightbulb"></i> Doctors </a>
+
+            <div id="menu-btn" onClick={toggleNavbar} className="fas fa-bars"></div>
+
+            <nav className="navbar">
+              <div id="close-navbar" className="fas fa-times" onClick={toggleNavbar}></div>
+                <Link to="/">Home</Link>
+                <Link to="/Doctors">Doctors</Link>
+                <Link to="">Leet</Link>
+                <Link to="">About</Link>
+                <Link to="">Contact</Link>
+            </nav>
+
+      </div>
+
+      <div id="sidebar" className={`sidebar ${isActive ? 'active' : ''}`}>
+        <div id="close-sidebar" className="fas fa-times" onClick={toggleNavbar}></div>
+
+        <Link to="/">Home</Link>
+        <Link to="/Doctors">Subject</Link>
+        <Link to="">Leet</Link>
+        <Link to="">About</Link>
+        <Link to="">Contact</Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar;
