b935cf8478111e25869b03de54231625e693fc03
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-20 19:00:54 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Service } from './pages'
 
 function App() {
 
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Service />} />
-        <Route path="/doctors/:pageNumber" element={<Home />} />
-        <Route path="/doctors" element={<Home />} />
-        <Route path="/Hospital" element={<Service />} />
-        <Route path="/About" element={<Service />} />
-        <Route path="/Contact" element={<Service />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Service />} />
+          <Route path="/doctors/:pageNumber" element={<Home />} />
+          <Route path="/doctors" element={<Home />} />
+          <Route path="/Hospital" element={<Service />} />
+          <Route path="/About" element={<Service />} />
+          <Route path="/Contact" element={<Service />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
